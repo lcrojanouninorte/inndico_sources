ba028cdef961a8a0e79bafe7712f2fa95b076443
Luis C <lcrojano@gmail.com>
2023-05-26 08:10:59 -0500

    Script 0 updated, read from databse and update post_processed database

--- scripts/update_post_processed.py
+++ scripts/update_post_processed.py
@@ -1,19 +1,38 @@
 from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import insert
 from credentials import *
 
-def update_post_processed(csv_data_source, raw_data_source_id):
+def update_post_processed(params):
     try:
-        # Crear el motor de SQLAlchemy
+        # Create the SQLAlchemy engine
         sqlalchemy_uri = "postgresql+psycopg2://inndico.bot:...Indico_Bot...@167.99.186.23:5432/ctinu_superset"
         engine = create_engine(sqlalchemy_uri)
 
-        # Construir la consulta de inserción
-        query = f"INSERT INTO {schema}.{post_processed_table_name} (csv_data_source, raw_data_source_id) VALUES ('{csv_data_source}', {raw_data_source_id})"
-        
-        # Ejecutar la consulta de inserción
-        engine.execute(query)
+        # Get the values from the dictionary
+        y_axis_label = params['y_axis_label']
+        x_axis_label = params['x_axis_label']
+        groupby = params['groupby']
+        csv_data_source = params['csv_data_source']
+        raw_data_source_id = params['raw_data_source_id']
+
+        # Build the insert statement
+        table = f"{schema}.{post_processed_table_name}"
+        insert_stmt = insert(table).values(
+            groupby=groupby,
+            x_axis_label=x_axis_label,
+            y_axis_label=y_axis_label,
+            csv_data_source=csv_data_source,
+            raw_data_source_id=raw_data_source_id
+        )
+
+        # Execute the insert statement
+        result = engine.execute(insert_stmt)
+
+        # Show the result of the query
+        print("SQL query executed successfully.")
+        print("A new record was inserted.")
 
         return 0
     except Exception as e:
-        print(f"Error al actualizar los datos procesados: {str(e)}")
+        print(f"Error: {str(e)}")
         return -1
